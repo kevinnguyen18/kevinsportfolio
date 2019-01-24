9dd9e7ab0cd66457ea901a7566cca786f9ff673b
dontflagme <ubermunchpunch@gmail.com>
2019-01-23 21:04:56 -0800

    Added user interface, added words file, added model to read from files

---
+++ formExample/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using formExample.Models;
+
+namespace formExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public WordsModel _dictionaryModel = new WordsModel();
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            //_dictionaryModel.ReadFile(@"\files\DictionaryWords.html");
+            _dictionaryModel.ArrayListOfSpecificWords = null;
+            return View(_dictionaryModel);
+            
+        }
+
+        [HttpPost]
+        public ActionResult Index(String letters)
+        {
+            int numberOfCharacters = letters.Length;
+            _dictionaryModel.ReadFile(@"\files\DictionaryWords.html", letters);
+            _dictionaryModel.ArrayListOfSpecificWords = _dictionaryModel.matchingWordsArray;
+
+
+            //return View(_dictionaryModel);
+            return View(_dictionaryModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+
+    }
+}
---
+++ formExample/Models/WordsModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.IO;
+
+namespace formExample.Models
+{
+    public class WordsModel
+    {
+        public ArrayList ArrayListOfSpecificWords { get; set; }
+        public ArrayList matchingWordsArray = new ArrayList();
+
+        /*
+        * @param - Takes in a file name as a string and attaches it to the current Directory to read from a file
+        */
+        public string ReadFile(string folderAndFileName, string userStringInput)
+        {
+
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+            string path = Directory.GetCurrentDirectory() + folderAndFileName;
+            //return path;
+            try
+            {
+
+                using (StreamReader lineReader = new StreamReader(path))
+                {
+                    string currentLine = null;
+                    string tempLine = null;
+                    while ((currentLine = lineReader.ReadLine()) != null)
+                    {
+                        tempLine = currentLine;
+                        matchingWordsArray.Add(tempLine); //Add to allWordsArray
+                    }
+                    return tempLine;
+
+                }
+            }
+
+            catch (Exception exception)
+            {
+
+                return "The file could not be read.";
+            }
+        }
+
+        /*
+         * Returns an array of matching words
+         */
+        public ArrayList getArray()
+        {
+            return matchingWordsArray;
+        }
+    }
+}
+ 
